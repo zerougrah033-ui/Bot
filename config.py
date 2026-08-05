dab846d70d64e232ec4b510ae8f4503933154e43
zerougrah033-ui <zerougrah033@gmail.com>
2026-08-05 20:59:25 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ TOKEN = os.getenv("TOKEN")
 HF_TOKEN = os.getenv("HF_TOKEN")
 
 
-MODEL_NAME = "unitary/multilingual-toxic-xlm-roberta"
+MODEL_NAME = "akhooli/xlm-r-large-arabic-toxic"
 LOG_CHANNEL_ID = 1525906374628741192
