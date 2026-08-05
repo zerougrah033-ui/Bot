6d0be637aab0ca28b11f779f2cd780055ada1713
zerougrah033-ui <zerougrah033@gmail.com>
2026-08-05 20:11:05 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ TOKEN = os.getenv("TOKEN")
 HF_TOKEN = os.getenv("HF_TOKEN")
 
 
-MODEL = "akhooli/xlm-r-large-arabic-toxic"
+MODEL_NAME = "sagorsarker/mbert-unseen-sentiment-analysis"
 LOG_CHANNEL_ID = 1525906374628741192
