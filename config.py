fc67f623ea1eba2e3354413e832de5ea4843af01
zerougrah033-ui <zerougrah033@gmail.com>
2026-08-05 21:03:05 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ TOKEN = os.getenv("TOKEN")
 HF_TOKEN = os.getenv("HF_TOKEN")
 
 
-MODEL_NAME = "akhooli/xlm-r-large-arabic-toxic"
+MODEL_NAME = "hossam87/bert-base-arabic-hate-speech"
 LOG_CHANNEL_ID = 1525906374628741192
